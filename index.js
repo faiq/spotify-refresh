6f9b89e0bf99775be37d3f56431dca37b73b2a8f
faiq <faiqrazarizvi@gmail.com>
2015-02-16 18:41:08 -0500

    dunno how to javascript

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = function (refreshToken, clientID, clientSecret, next) {
   var opts = {
     uri: 'https://accounts.spotify.com/api/token',
     json: {
-      grant_type: 'refresh_token', 
-      refresh_token: refreshToken
-      client_id: clientID, 
-      client_secret: clientSecret
+      'grant_type': 'refresh_token', 
+      'refresh_token': refreshToken
+      'client_id': clientID, 
+      'client_secret': clientSecret
     }
   } 
   return request(opts, next)
