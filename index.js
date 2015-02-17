0af8d5d4329aa6008ff43a57cd05adcfb6991cb6
faiq <faiqrazarizvi@gmail.com>
2015-02-17 02:39:39 -0500

    i am a fool

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var request = require('request')
 
 module.exports = function (refreshToken, clientID, clientSecret, next) { 
-  var opts = {
+  var auth = 'Basic ' +  new Buffer(clientID + ':' + clientSecret).toString('base64')
+    , opts = {
     uri: 'https://accounts.spotify.com/api/token',
+    method: 'POST',
     json: {
       'grant_type': 'refresh_token', 
       'refresh_token': refreshToken
     },
     headers: { 
-      'Authorization': 'Basic ' +  new Buffer(clientID + ':' + clientSecret).toString('base64')
+      'Authorization': auth
     }
   } 
   return request(opts, next)
